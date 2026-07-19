1810963002700be2f358512af70c3780fe5624ba
ljwagner0978 <lswagner24@gmail.com>
2026-07-19 13:00:22 -0400

    Update assignment-1.py

--- assignment-1.py
+++ assignment-1.py
@@ -1,26 +1,26 @@
 # Lewis Wagner
 # Assignment 1: Profile Card Builder
-# Section 1
 
+import datetime
+
+# Section 1
 first_name = "John"
 last_name = "Hubert"
 age = 41
-occupation = 'Janitor'
 weight_lbs = 197.6
 is_married = True
 
 print(first_name, type(first_name))
 print(last_name, type(last_name))
 print(age, type(age))
-print(occupation,type(occupation))
 print(weight_lbs, type(weight_lbs))
 print(is_married, type(is_married))
 
 # Section 2
-
+date = datetime.datetime.now()
 x = input("Enter your first name: ")
 y = input("Enter the year you were born: ")
-print(f"Hi, {x}! You are approximately {str((2026 - int(y)))} years old.")
+print(f"Hi, {x}! You are approximately {str((date.year - int(y)))} years old.")
 
 # Section 3
 
@@ -34,32 +34,19 @@ print(f"{number_1} × {number_2} = {result}")
 
 membership_status = True
 food_item = "Diet Coke Cola Soda - 12 pk"
-regular_food_price = 10.99
-
-def membership_price(regular_food_price, membership_status):
-  if(membership_status):
-      return(8.99)
-  else:
-      return regular_food_price
-      
-actual_food_price = membership_price(regular_food_price, membership_status)
+food_price = 10.99
 quantity = 2
-total_cost = round(actual_food_price * quantity, 2)
-total_currency_formatted = f"${total_cost:,.2f}"
+total_cost = round((food_price * quantity), 2)
 
 print("===========================")
 print("        RECEIPT            ")
 print("===========================")
 
 print(f"Item: {food_item}")
-print(f"Price: ${actual_food_price}")
+print(f"Price: ${food_price}")
 print(f"Quantity: {quantity}")
 print("---------------------------")
-print(f"Total:   {total_currency_formatted}")
-print("---------------------------")
-if(regular_food_price - actual_food_price > 0):
-    print("Thanks for being a member!")
-    print(f"You saved ${((regular_food_price - actual_food_price) * quantity):,.2f} with us today!")
+print(f"Total:   {total_cost}")
 print("===========================")
 
 # Section 5
@@ -71,9 +58,9 @@ fun_fact_s5 = input("Please provide one fun fact about yourself: ")
 birth_year_s5 = input("Please provide your birth year: ")
 
 print("╔══════════════════════════════╗")
-print(f"       PROFILE: {name_s5}         ")
+print(f"     PROFILE: {name_s5}        ")
 print("╚══════════════════════════════╝")
 print(f" Hometown:  {hometown_s5}")
 print(f" Hobby:     {hobby_s5}")
 print(f" Fun fact:  {fun_fact_s5}")
-print(f" Age:       {(2026 - int(birth_year_s5))}")
+print(f" Age:       {(date.year - int(birth_year_s5))}")
